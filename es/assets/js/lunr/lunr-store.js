49ce6cd96f294b02558b1248775ce47df0fd446c
dev <user@users.noreply.github.com>
2021-06-10 18:30:56 +0300

    inital

---
+++ es/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Argumentos de la línea de comandos de CGMiner [2021]",
+        "excerpt":"You can use this list as a reference for all Launch Parameters supported by CGMiner.    You can download CGMiner 3.7.2 from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace CGMiner.exe with ./CGMiner in the command-line examples below.   ","categories": ["documentation"],
+        "tags": ["CGMiner"],
+        "url": "https://cgminer.info/documentation/setup/",
+        "teaser": "https://cgminer.info/assets/images/thumbs/500x300.png"
+      },{
+        "title": "Download CGMiner  3.7.2 - AMD GPU Miner [2021]",
+        "excerpt":" Get started with CGMiner  \t\t \t\t\t \t\tVersion: 3.7.2  \t\tFile: *cgminer.exe,    \t\tSHA256: 417b22681a716e2481fde1fcaed96d66a5716a188186a4ae765d558d4a7ec2f5  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new CGMiner stale version is ready.   You can download CGMiner 3.7.2 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "tags": ["CGMiner"],
+        "url": "https://cgminer.info/ru/download/37.5/",
+        "teaser": "https://cgminer.info/assets/images/thumbs/500x300.png"
+      },]
---
+++ ru/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "CGMiner&#58; Комиссия, Системные Требования и Лимиты [2021]",
+        "excerpt":"CGMiner требует Windows x64 (Windows 7, Windows 10 и т.д.) Или Linux x64 (проверено на Ubuntu LTS и стабильной версии Debian)    Для начала вы можете скачать самую последнюю версию CGMiner 3.7.2 отсюда:   Скачать сейчас   Скачать (зеркало)   CGMiner имеет интерфейс командной строки. Для минимальной конфигурации введите в .bat файле:   CGMiner -a [алгоритм майнинга] -o [сервер пула] -u [имя пользователя или адрес кошелька]   Пример:   CGMiner -a ethash -o ethproxy+tcp://asia1.ethermine.org:4444 -u 0x12343bdgf.worker   Для получения полного списка параметров конфигурации выполните:   CGMiner –help   Готовые шаблоны   ","categories": ["documentation"],
+        "tags": ["CGMiner"],
+        "url": "https://cgminer.info/documentation/setup/",
+        "teaser": "https://cgminer.info/assets/images/thumbs/500x300.png"
+      },{
+        "title": "Скачать CGMiner  3.7.2 - AMD GPU Miner [2021]",
+        "excerpt":" Начните добывать криптовалюту   с помощью CGMiner  \t\t \t\t\t \t\tВерсия: 3.7.2  \t\tФайл: *cgminer.exe,   \t\tSHA256: 417b22681a716e2481fde1fcaed96d66a5716a188186a4ae765d558d4a7ec2f5  \t\t(Скачать для Linux)\t \t\t \t\t \t\t \t\t \t\tСкачать сейчас(Windows x64) \t\t \t\t(зеркало) \t\t \t     The new CGMiner stale version is ready.   You can download CGMiner 3.7.2 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "tags": ["CGMiner"],
+        "url": "https://cgminer.info/ru/download/37.5/",
+        "teaser": "https://cgminer.info/assets/images/thumbs/500x300.png"
+      },{
+        "title": "CGMiner&#58; Настройки, Команды, Параметры Командной Строки",
+        "excerpt":"**    Скачать   Скачать   Параметры командной строки：   CGMiner -a algo -o protocol+socket_type://pool_host:pool_port -u wallet_address.worker:passwd  ","categories": ["documentation"],
+        "tags": ["CGMiner"],
+        "url": "https://cgminer.info/documentation/arguments/",
+        "teaser": "https://cgminer.info/assets/images/thumbs/500x300.png"
+      },{
+        "title": "Clear 06 10",
+        "excerpt":"   ","categories": ["articles"],
+        "tags": [],
+        "url": "https://cgminer.info/ru/articles/clear-06-10/",
+        "teaser": "https://cgminer.info/assets/images/thumbs/500x300.png"
+      },]
